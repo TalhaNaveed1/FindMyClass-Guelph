c1905804d9850308eb0f16960e38f2c26bb351a6
TalhaNaveed1 <naveedtalha11@gmail.com>
2024-07-07 02:20:29 -0400

    Added Footer

---
+++ findmyclass/src/app/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="flex bg-black">
+            <p className="text-white">© 2024 Find My Class. All rights reserved.</p>
+        </footer>
+    );
+}
