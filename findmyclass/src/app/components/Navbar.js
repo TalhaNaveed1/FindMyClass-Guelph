9c63a53b40fac08396ebf4a1be82c24015814ec7
TalhaNaveed1 <naveedtalha11@gmail.com>
2024-07-07 16:02:42 -0400

    Added hover effect

--- findmyclass/src/app/components/Navbar.js
+++ findmyclass/src/app/components/Navbar.js
@@ -4,8 +4,8 @@ export default function Navbar() {
             <nav className="flex flex-row justify-between">
                 <button className="flex flex-row text-red-600 font-sans font-bold text-3xl ml-5 ">FindMy<p className="text-yellow-300"><em>Class</em></p></button>
                 <div className="flex flex-row justify-around">
-                    <button className="text-yellow-300 border-yellow-300 font-sans border-2 rounded p-2 pr-3 pl-3 mr-5 font-semibold">Sign up</button>
-                    <button className="text-red-600 font-sans mr-5 border-2 rounded p-2 pr-3 pl-3 border-red-600 font-semibold">Sign in</button>
+                    <button className="text-yellow-300 border-yellow-300 font-sans border-2 rounded p-2 pr-3 pl-3 mr-5 font-semibold hover:bg-yellow-300 trasnition-colors hover:text-black transition-colors">Sign up</button>
+                    <button className="text-red-600 font-sans mr-5 border-2 rounded p-2 pr-3 pl-3 border-red-600 font-semibold hover:bg-red-600 trasnition-colors hover:text-black transition-colors">Sign in</button>
                 </div>
             </nav>
         </div>
