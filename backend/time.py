ece19d8cb8e8c754d450b7bbe5e99cea4a128029
TalhaNaveed1 <naveedtalha11@gmail.com>
2024-08-21 01:17:54 -0400

    Initialized time.py

---
+++ backend/time.py
@@ -0,0 +1,18 @@
+import math
+import pandas
+import numpy
+
+def read_data(data):
+    print("Testing read_data")
+    file_path = "files/data.csv"
+    data = pandas.read_csv(file_path)
+    return
+
+def euclidean_distance(p1,p2):
+    print("Euclidean Distance")
+    return
+
+def k_nearest_neighbours(data,p,k):
+    print("Testing k_nearest_neighbours")
+    return
+
