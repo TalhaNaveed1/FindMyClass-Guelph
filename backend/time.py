871dbc7f77943b9ae0c03fbf75a57cfe1018b9fc
TalhaNaveed1 <naveedtalha11@gmail.com>
2024-08-22 02:25:15 -0400

    Fixed data reading and edited csv

--- backend/time.py
+++ backend/time.py
@@ -3,6 +3,9 @@ import pandas
 import numpy
 
 def read_data():
+    # Day of Week: Monday: 0, Sunday: 6
+    # Month: January:0, December: 11
+    # Traffic Level: Low: 0, Mid: 1, High: 2
     print("Testing read_data")
     file_path = "files/data.csv"
     data_path = pandas.read_csv(file_path)
