1c1359feca64a051cf4056580ad4e0abdb5b68d6
TalhaNaveed1 <naveedtalha11@gmail.com>
2024-08-23 00:56:14 -0400

    Attempting KNN

--- backend/time.py
+++ backend/time.py
@@ -6,7 +6,6 @@ def read_data():
     # Day of Week: Monday: 0, Sunday: 6
     # Month: January:0, December: 11
     # Traffic Level: Low: 0, Mid: 1, High: 2
-    print("Testing read_data")
     file_path = "files/data.csv"
     data_path = pandas.read_csv(file_path)
     data = data_path.values.tolist()
@@ -14,7 +13,6 @@ def read_data():
     return data
 
 def euclidean_distance(p1,p2):
-    print("Euclidean Distance")
     EC_distance = 0
     for index in range(len(p1) - 2):
         EC_distance += (p1[index] - p2[index]) ** 2
@@ -24,9 +22,21 @@ def euclidean_distance(p1,p2):
 
 def k_nearest_neighbours(data,p,k):
     print("Testing k_nearest_neighbours")
-    return
+    EC_distances = []
+    for point in data:
+        temp_distance = euclidean_distance(p,point)
+        EC_distances.append((point,temp_distance))
+    
+    for dist in EC_distances:
+        print(dist)
+    print('\n')
+    EC_distances.sort(key=lambda x: x[0])
+    for dist in EC_distances:
+        print(dist)
 
 
 myData = read_data()
-myEuclideanDistance = euclidean_distance(myData[0],myData[1])
-print(myEuclideanDistance)
+test = [8,0,0]
+k = 3
+k_nearest_neighbours(myData,test,k)
+
