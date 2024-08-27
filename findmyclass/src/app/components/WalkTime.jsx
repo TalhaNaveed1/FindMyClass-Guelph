3d533f8c1b1ead458a61c39853aba4aaa3fc17f8
TalhaNaveed1 <naveedtalha11@gmail.com>
2024-08-27 00:27:08 -0400

    Fixed WorldTimeAPI issue

--- findmyclass/src/app/components/WalkTime.jsx
+++ findmyclass/src/app/components/WalkTime.jsx
@@ -32,11 +32,15 @@ export default function WalkTime() {
 
     return (
         <div className="flex flex-col bg-black p-12 rounded-xl items-center">
-            <img src="/traffic_images/high.png" alt="traffic_display" className="w-[200px] h-[150px]" />
-            <p className="text-white text-center text-xl mt-4 max-w-[80%] md:max-w-[300px] mx-auto">
-  At 8:34pm, we estimate it may take you <em><b><u>longer</u></b></em> than average to reach your class.
-</p>
-            {/* <h2 className="text-white text-2xl">{prediction}</h2> */}
+            {prediction ? (
+                <>
+                    <img src={imgSrc} alt="traffic_display" className="w-[200px] h-[150px]" />
+                    <p className="text-white text-center text-2xl font-semibold mt-4 max-w-[80%] md:max-w-[300px] mx-auto">{description}</p>
+                </>
+            ) : (
+                <p className="text-white text-center text-xl mt-4 font-semibold font-sans">Loading prediction...</p>
+            )}
         </div>
-    )
+    );
+    
 }
--- findmyclass/src/app/utils/fetchWalkTime.js
+++ findmyclass/src/app/utils/fetchWalkTime.js
@@ -2,13 +2,13 @@ export async function fetchWalkTime() {
   const apiResponse = await fetch("http://worldtimeapi.org/api/timezone/America/Toronto");
   const timeData = await apiResponse.json();
   const datetime = new Date(timeData.datetime);
-  const dayOfWeek = datetime.getUTCDay();
-  const month = datetime.getUTCMonth();
-  const hour = datetime.getUTCHours();
+  const dayOfWeek = datetime.getDay();
+  const month = datetime.getMonth();
+  const hour = datetime.getHours();
 
   console.log(dayOfWeek);
   console.log(month);
-  console.log(hour);
+  console.log('hour',hour);
 
   const backendResponse = await fetch("http://127.0.0.1:8080/predict", {
     method: "POST",
