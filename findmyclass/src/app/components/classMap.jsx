3c423b8e72424b297359932bf5b9443cec9bfeed
TalhaNaveed1 <naveedtalha11@gmail.com>
2024-08-28 00:41:39 -0400

    Added different coordinates

--- findmyclass/src/app/components/classMap.jsx
+++ findmyclass/src/app/components/classMap.jsx
@@ -4,6 +4,28 @@ import { GoogleMap, useJsApiLoader, MarkerF } from "@react-google-maps/api";
 const ClassMap = ({ mapsCode }) => {
   console.log(mapsCode);
 
+  const classCoordinates = {
+    "ALEX": { lat: 43.5293, lng: -80.2277 },
+    "THRN": { lat: 43.5312, lng: -80.2255 },
+    "ROZH": { lat: 43.5322, lng: -80.2259 },
+    "ANNU": { lat: 43.5296, lng: -80.2297 },
+    "MACN": { lat: 43.5296, lng: -80.2297 },
+  };
+  let coordinates = '';
+
+  if (mapsCode.slice(0,2) == 'FS') {
+    coordinates = { lat: 43.5298, lng: -80.2310 }
+  }
+  else if (mapsCode.slice(0,2) == 'LA') {
+    coordinates = { lat: 43.5324, lng: -80.2252 }
+  }
+  else if (mapsCode.slice(0,2) == 'ZAV') {
+    coordinates = { lat: 43.5310, lng: -80.2272 }
+  }
+  else {
+    coordinates = classCoordinates[mapsCode.slice(0,4)]
+  }
+
   const containerStyle = {
     width: "400px",
     height: "300px",
