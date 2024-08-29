4eb0190363fed89720024c672bb98ddf8c4e5dae
TalhaNaveed1 <naveedtalha11@gmail.com>
2024-08-29 00:41:12 -0400

    Added shadcn accordian

--- findmyclass/src/app/allClasses/page.js
+++ findmyclass/src/app/allClasses/page.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
 export default function AllClasses() {
     return (
         <main className="flex flex-col justify-center items-center min-h-screen bg-johnston bg-cover bg-center w-full">
                 <div>
-                    <h1 className="text-white text-4xl font-semibold">Testing</h1>
+                <h1 className="text-white font-sans font-bold text-8xl">The Full Class List</h1>
                 </div>
         </main>
     );
