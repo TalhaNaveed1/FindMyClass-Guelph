89696a43498751d39fd75a22983f79a968ba7358
TalhaNaveed1 <naveedtalha11@gmail.com>
2024-09-11 20:56:30 -0400

    Added responsive changes to main page

--- findmyclass/src/app/page.js
+++ findmyclass/src/app/page.js
@@ -64,41 +64,44 @@ export default function Page() {
   };
 
   return (
-    <main className="flex flex-col justify-center items-center min-h-screen bg-johnston bg-cover bg-center w-full">
+    <main className="flex flex-col justify-center items-center min-h-screen bg-johnston bg-cover bg-center w-full px-4 md:px-8">
       <div className="flex flex-col justify-center items-center text-center slide-in">
-        <div className="flex flex-row mt-10">
-          <h1 className="text-white font-sans font-bold text-8xl slide-in">FindMy</h1>
-          <h1 className="text-blue-700 font-sans font-bold text-8xl slide-in">
+        <div className="flex">
+          <h1 className="text-white font-sans font-bold text-5xl md:text-8xl slide-in">
+            FindMy
+          </h1>
+          <h1 className="text-blue-700 font-sans font-bold text-5xl md:text-8xl slide-in">
             <em>Class</em>
           </h1>
           <img
             src="/images/magnifiying.gif"
             alt="Magnifiying GIF"
-            className="h-40 pb-15 slide-in"
+            className="h-20 md:h-40 pb-15 slide-in"
           />
         </div>
-        <h3 className="text-white font-sans font-bold mb-4 slide-in">
+        <h3 className="text-white font-sans font-bold text-sm md:text-2xl mb-4 slide-in">
           <em>
-            A step-by-step guide to every class at the University of Guelph
+            Your guide to every class at the University of Guelph
           </em>
         </h3>
-        <form className="flex slide-in" onSubmit={handleSubmit}>
+        <form className="flex slide-in w-full max-w-xs md:max-w-md" onSubmit={handleSubmit}>
           <input
             type="text"
-            className="p-3 w-96 h-16 rounded-xl"
+            className="p-3 w-full h-12 md:h-16 rounded-xl"
             placeholder="Enter a class code..."
             onChange={updateInput}
             value={inputValue}
           />
         </form>
 
-        <Link href="/allClasses" className="text-white font-sans font-bold mt-20 relative slide-in">
+        <Link href="/allClasses" className="text-white font-sans font-bold mt-10 md:mt-20 relative slide-in">
           View all classes here &gt;&gt;&gt;
         </Link>
       </div>
       {masterList.length > 0 && (
-        <PathDisplay key={myKey} masterList={masterList} mapsCode={mapsCode} className="slide-in"/>
+        <PathDisplay key={myKey} masterList={masterList} mapsCode={mapsCode} className="slide-in" />
       )}
     </main>
   );
 }
+
