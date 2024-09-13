16c18a7508062509a1605014db8dd8da85ddfc6a
TalhaNaveed1 <naveedtalha11@gmail.com>
2024-09-13 02:17:16 -0400

    Switched to hosted backend

--- findmyclass/src/app/utils/fetchWalkTime.js
+++ findmyclass/src/app/utils/fetchWalkTime.js
@@ -1,21 +1,17 @@
 export async function fetchWalkTime() {
-  console.log("pre a");
   const apiResponse = await fetch("http://worldtimeapi.org/api/timezone/America/Toronto");
-  console.log("a");
   const timeData = await apiResponse.json();
-  console.log("b");
   const datetime = new Date(timeData.datetime);
   const dayOfWeek = datetime.getDay();
   const month = datetime.getMonth();
   const hour = datetime.getHours();
   const displayTime = datetime.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
-  console.log("c");
 
   console.log(dayOfWeek);
   console.log(month);
   console.log('hour',hour);
 
-  const backendResponse = await fetch("http://127.0.0.1:8080/predict", {
+  const backendResponse = await fetch("https://talhanaveedfmcg.pythonanywhere.com/predict", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -26,11 +22,9 @@ export async function fetchWalkTime() {
       hour: hour,
     }),
   });
-  console.log("d");
 
   const predictionData = await backendResponse.json();
   console.log(predictionData)
-  console.log("e");
   return {
     predictedLevel: predictionData.predictedLevel,
     currentTime: displayTime
